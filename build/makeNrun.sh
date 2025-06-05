e623790108078df627d049d992b60c0f2ac1e787
home <home@arch.com>
2025-06-05 13:57:10 +0200

    Organized files

---
+++ build/makeNrun.sh
@@ -0,0 +1,7 @@
+i386-elf-gcc -ffreestanding -m32 -g -c ../src/kernel.c -o kernel.o
+nasm ../src/kernel_entry.asm -f elf -o kernel_entry.o
+i386-elf-ld -o full_kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
+nasm ../src/boot.asm -f bin -o boot.bin
+cat boot.bin full_kernel.bin >everything.bin
+cat everything.bin zeroes.bin >OS.bin
+qemu-system-x86_64 -drive format=raw,file="OS.bin",index=0,if=floppy -m 128M
