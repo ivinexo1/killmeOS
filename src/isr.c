d0cf76a8c08e96e48b07a610db49f793edd07e15
Kravataf <159631603+Kravataf@users.noreply.github.com>
2025-06-09 18:34:35 +0200

    hweh

--- src/isr.c
+++ src/isr.c
@@ -34,22 +34,6 @@ void isr_install() {
   set_idt_gate(45, (uint32_t)irq13);
   set_idt_gate(46, (uint32_t)irq14);
   set_idt_gate(47, (uint32_t)irq15);
-
-  // ICW1
-  port_byte_out(0x20, 0x11);
-  port_byte_out(0xA0, 0x11);
-  // ICW2
-  port_byte_out(0x21, 0x20);
-  port_byte_out(0xA1, 0x28);
-  // ICW3
-  port_byte_out(0x21, 0x04);
-  port_byte_out(0xA1, 0x02);
-  // ICW4
-  port_byte_out(0x21, 0x01);
-  port_byte_out(0xA1, 0x01);
-  // OCW1
-  port_byte_out(0x21, 0x0);
-  port_byte_out(0xA1, 0x0);
   
   load_idt();
 }
